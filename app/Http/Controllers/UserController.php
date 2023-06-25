46c94d5f28547c50b3e517a35ce80c669f4f30ba
Rere <rennyadr27@gmail.com>
2023-06-25 12:30:25 +0700

    inisialisasi projek

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function pertambahan(){
+        return "Ini Halaman Pertambahan";
+    }
+
+    public function perkalian($angka1, $angka2) {
+        $num1 = (int)$angka1;
+        $num2 = (int)$angka2;
+        $hasil = $num1 * $num2;
+        return "Hasilnya: {$hasil}";
+    }
+}
+
